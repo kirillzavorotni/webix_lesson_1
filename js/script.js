3414fcfa8c792d43601a93e011999a0b58fb4cb0
Kiryl Zavarotny <kirillzavorotni@gmail.com>
2019-01-30 22:38:22 +0300

    feat: add first row (label and button) in app

--- js/script.js
+++ js/script.js
@@ -1,5 +1,33 @@
 webix.ready(function(){
+  const firstRow = {
+    cols: [
+      { 
+        view: 'label',
+        label: 'My App',
+        css: 'custom-label-style',
+        width: 75,
+      },
+      { view: 'spacer' },
+      {
+        view: 'button',
+        type: 'icon',
+        icon: 'mdi mdi-account',
+        id: 'profile',
+        label: 'Profile',
+        width: 90,
+        css: 'transparent_button',
+      },
+    ],
+    css: 'first_row',
+    height: 40,
+  };
+
   webix.ui({
-      
+      view: 'layout',
+      rows: [
+        firstRow,
+        {},
+        {},
+      ],
   });
 });
